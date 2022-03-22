e54c4ab9b9dc984ea16c5250bbeeb2d79506c0fe
iDuqueZ <ivandariduque@gmail.com>
2022-03-22 12:51:50 -0500

    Corrección

--- aplicacion/src/components/basicList.jsx
+++ aplicacion/src/components/basicList.jsx
@@ -5,7 +5,7 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
+
 
 
 export default function BasicList() {
