eb463728a141dfbea7d9344025c89771545877f6
iDuqueZ <71943755+iDuqueZ@users.noreply.github.com>
2022-03-30 15:35:15 -0500

    Efecto de seleccionar producto

--- aplicacion/src/components/Productos.jsx
+++ aplicacion/src/components/Productos.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Row, Col, Card} from 'react-bootstrap'
+import '../styles/listarProductos.css'
 
 export default function Productos() {
   return (
     <Row style={{marginRight: '0'}} xs={1} md={4} className="g-4">
     {Array.from({ length: 6 }).map((_, idx) => (
       <Col>
-        <Card style={{width: '14rem', margin: 'auto'}}>
+        <Card className='Card' style={{width: '14rem', margin: 'auto'}}>
           <Card.Img style={{sobjectFit: 'cover', backgroundSize: 'cover'}} variant="top" src="https://esika.tiendabelcorp.com/cdn-cgi/image/width=1200,fit=contain,f=auto/https://belc-bigdata-mdm-images-prd.s3.amazonaws.com/images/FotoProductoFondoBlancoWebRedes/210097326-FotoFondoBlanco-v02.jpg" />
           <Card.Body>
             <Card.Title>Pruebas</Card.Title>
